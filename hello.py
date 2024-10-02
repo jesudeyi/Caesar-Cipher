e87d05d9a5609562bbbfc447b432c0bcd255ae3a
David Fadeyi <fadav4real@gmail.com>
2024-10-02 04:17:43 +0100

    Caeser cipher

---
+++ hello.py
@@ -0,0 +1,40 @@
+# Caeser_cipher
+def caesar_cipher(text, shift):
+        result = ""
+        
+        for char in text:
+            if char.isupper():
+                result += chr((ord(char) + shift - 65) % 26 + 65)
+            elif char.islower():
+                result += chr((ord(char) + shift - 97) % 26 + 97)
+            else:
+                result += char
+        
+        return result
+option = 'y'
+while option == 'y' or option == 'Y':
+    try: 
+        # Welcome instructions
+        print("Which operation do you wish to perform?")
+        print("1. Encrypting a message")
+        print("2. Decrypting a message") 
+        choice = int(input("Enter an option: "))  
+        if choice == 1:
+            plainText = input("Enter the message you want to encrypt: ")
+            key = int(input("Enter your desired encryption key (shift value): "))
+            encryptedText = caesar_cipher(plainText, key)
+            print(encryptedText)
+        elif choice == 2:
+            encryptedText = input("Enter the endoded message you want to decrypt: ")
+            key = int(input("Enter the correct decryption key (shift value): "))
+            reversedKey = key * -1
+            decryptedText = caesar_cipher(encryptedText, reversedKey)
+            print(decryptedText)
+        else:
+            print("Input an valid option 1 or 2")
+    except ValueError:  
+        print("That's not a valid number!")
+    print("Do you want to perform another Caeser operation? y/n")
+    option = input()
+
+
